7d2726fd68cdc398463b592eab0f4e76dcda22bb
F Jiang <jiang.tf.2020@gmail.com>
2023-03-22 19:45:55 +0000

    Something to see here

--- assets/js/permalimbo.js
+++ assets/js/permalimbo.js
@@ -1,7 +1,5 @@
 function foo() {
-  const ball = document.getElementById("my-ball");
-  ball.style.left = "300px";
-  ball.style.top = "300px";
+  console.log("There is something to see here.");
 }
 
 window.addEventListener("load", foo);
